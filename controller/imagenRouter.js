533f7630cf7059efcb46d1ff8d84891e375a8b1d
pSenciales <124668501+pSenciales@users.noreply.github.com>
2024-12-11 11:07:03 +0100

    no habia puesto '/'

--- controller/imagenRouter.js
+++ controller/imagenRouter.js
@@ -53,7 +53,7 @@ router.post("/", async (req, res) => {
                 }
 
                 // Hacer la petición PUT a tu API
-                const apiResponse = await axios.put(`https://parcial-back-seven.vercel.app/articulos/imagen${id}`, {
+                const apiResponse = await axios.put(`https://parcial-back-seven.vercel.app/articulos/imagen/${id}`, {
                     url: uploadResult.secure_url,
                     descripcion: descripcion,
                 });
