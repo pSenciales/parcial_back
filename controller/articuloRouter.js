5747110825a38921cef5262192e953769a2401de
pSenciales <124668501+pSenciales@users.noreply.github.com>
2024-12-12 13:54:04 +0100

    cambio ruta de borrado

--- controller/articuloRouter.js
+++ controller/articuloRouter.js
@@ -132,7 +132,7 @@ router.put("/:id", async (req, res) => {
 })
 
 
-router.delete("/:id", async (req, res) => {
+router.delete("/articulo/:id", async (req, res) => {
     try {
         const { id } = req.params;
         const articuloEliminado = await Articulo.findByIdAndDelete(id);
